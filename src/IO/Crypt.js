fd032c85b5c7de63ede3b378011d66d0fdd2fc7f
b.ahlborn <bernd.ahlborn@vilua.de>
2019-10-30 10:13:02 +0100

    Forgot to add Crypt.js and also added basic .gitignore

---
+++ src/IO/Crypt.js
@@ -0,0 +1,48 @@
+import fs from 'fs';
+import path from 'path';
+import * as openpgp from 'openpgp';
+let propertyPath = process.argv[2];
+let property = require(propertyPath).property;
+let namespace = property.namespace;
+let basePath = path.dirname(propertyPath)+'/';
+
+
+import FileReader from '../IO/FileReader';
+
+class Crypt {
+	constructor() {
+		this.password = this.readPassword()
+	}
+
+
+
+	readPassword(){
+		try{
+			let password = FileReader.read(property.cryptAbsolutePath)	
+			return password;
+		}
+		catch (e) {
+			console.log("Cannot read encryption password file at",property.cryptAbsolutePath)
+			console.log(e.stack)
+		}
+		
+	}
+
+	encrypt(data){
+		try {
+			let options = {};
+			options.message = openpgp.message.fromText(data);
+			options.passwords = this.password;
+			options.armor = false;
+			let cryptmessage = openpgp.encrypt(options).then(encrypted => {return encrypted.message.packets.write()})
+			return cryptmessage;
+		}
+		catch (e) {
+			console.log("Error while encryption")
+			console.log(e.stack)
+		}
+	}
+
+}
+
+export default (new Crypt());
